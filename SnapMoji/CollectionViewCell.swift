eb158f1406c8332698edcb068eb2c20b579e7f3e
Jiung Choi <jiungc@sfu.ca>
2018-10-26 17:29:31 -0700

    First draft of UI

---
+++ SnapMoji/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  SnapMoji
+//
+//  Created by Jiung Choi on 10/26/18.
+//  Copyright © 2018 Mojo Mojis. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var tommyProfile: UIImageView!
+    @IBOutlet weak var tommyLabel: UILabel!
+}
--- SnapMoji/ViewController.swift
+++ SnapMoji/ViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let friends = ["Tommy"]
+    
+    let friendImages: [UIImage] = [
+        UIImage(named: "sadface")!
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
+       // collectionView.dataSource = self
+      //  collectionView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +27,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return friends.count
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        cell.tommyLabel.text=friends[indexPath.item]
+        cell.tommyProfile.image=friendImages[indexPath.item]
+        return cell
+    }
 
 }
 
