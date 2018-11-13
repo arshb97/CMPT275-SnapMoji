7b74f9d6181aafd60f3ac00f5ba3ed4cf580aeb8
Josh Baltar <joshbaltar@gmail.com>
2018-11-13 14:01:47 -0800

    fixed size of some UI back buttons

--- SnapMoji/TestingViewController.swift
+++ SnapMoji/TestingViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class TestingViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
