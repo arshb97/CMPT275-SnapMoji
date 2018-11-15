0dcd0b1bad765c65d3b087f8d157a969f7109c5a
mirnazaki <mzaki@sfu.ca>
2018-11-14 17:40:07 -0800

    Label in the library of emotions set to the friends name when their button is pressed. bug of saving created friends fixed

--- SnapMoji/CollectionViewCell.swift
+++ SnapMoji/CollectionViewCell.swift
@@ -3,8 +3,7 @@
 //  SnapMoji
 //
 //  Description of CollectionViewCell:
-//  This file has not been implemented yet, but is for the display of the friends in the
-//  friend section
+//  Contains a button and a label for each friend that is created in the friend seciton
 //
 //
 //  Worked on by: Jiung Choi, Merna Zaki
@@ -13,6 +12,7 @@
 //  Changelog:
 //  1.0 - This is the initial commit of our view controller
 //  2.0 - added the items in the cell for the collection view
+//  2.1 - Implmented the friend button (saving the name of the friend to firebase)
 //
 //  Known bugs:
 //  There are no known bugs related to the ViewController.swift
@@ -22,8 +22,17 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class CollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var profileButton: UIButton! //Button with a proile pitcure
     @IBOutlet weak var friendName: UILabel! //label containing the friends name
+    
+    @IBAction func FriendButton_tab(_ sender: Any) {
+    //send the name of the friend pressed to Firebase
+     let ref = Database.database().reference()
+     ref.child("currentFriend/name").setValue(self.friendName.text)
+     print("NAME PRESSED: ", self.friendName.text ?? "")
+    }
 }
