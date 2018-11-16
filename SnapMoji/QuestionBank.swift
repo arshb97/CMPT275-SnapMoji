78513c5e6a0115a8311a5870292d5014d81f8bc8
Josh Baltar <joshbaltar@gmail.com>
2018-11-16 07:20:06 -0800

    randomly generates 5 questions using the sample images; does not currently filter used emotions

--- SnapMoji/QuestionBank.swift
+++ SnapMoji/QuestionBank.swift
@@ -11,7 +11,43 @@ import Foundation
 class QuestionBank{
     var list = [Question]()
     
+    var friend = "" //get friends from firebase
+    let sample = "Sample"
+    let emotions = ["happiness", "sadness", "anger", "surprise", "disgust", "fear", "contempt", "neutral"]
+    
+    //use sample if less than 5 images exist
+        //randomly pick an emotion and mark it to exclude it from the quiz
+    //var randInt1 = arc4random_uniform(8)
+    //var randInt2 = arc4random_uniform(8)
+    //var randInt3 = arc4random_uniform(8)
+    //var randInt4 = arc4random_uniform(8)
+    //var randInt5 = arc4random_uniform(8)
+    
+    
+    
+    var randAns = "noEmotion"
+    
+    init(randAns: String) {
+        //randAns = emotions[randInt]
+    }
+    
     init() {
-        list.append(Question(image: "happiness_emoji", questionText: "What emotion is this?", userChoiceA: "Happiness", userChoiceB: "Sadness", userChoiceC: "Anger", userChoiceD: "Surprise", answer: 1))
+        let randEmotion1 = emotions.randomElement()!
+        let randEmotion2 = emotions.randomElement()!
+        let randEmotion3 = emotions.randomElement()!
+        let randEmotion4 = emotions.randomElement()!
+        let randEmotion5 = emotions.randomElement()!
+        
+        let randAns1 = sample + randEmotion1
+        let randAns2 = sample + randEmotion2
+        let randAns3 = sample + randEmotion3
+        let randAns4 = sample + randEmotion4
+        let randAns5 = sample + randEmotion5
+        
+        list.append(Question(image: randAns1, questionText: "What emotion is this?", userChoiceA: randEmotion1, userChoiceB: "Sadness", userChoiceC: "Anger", userChoiceD: "Surprise", answer: 1))
+        list.append(Question(image: randAns2, questionText: "What emotion is this?", userChoiceA: "Happiness", userChoiceB: randEmotion2, userChoiceC: "Anger", userChoiceD: "Surprise", answer: 2))
+        list.append(Question(image: randAns3, questionText: "What emotion is this?", userChoiceA: "Happiness", userChoiceB: randEmotion3, userChoiceC: "Anger", userChoiceD: "Surprise", answer: 2))
+        list.append(Question(image: randAns4, questionText: "What emotion is this?", userChoiceA: "Happiness", userChoiceB: "Anger", userChoiceC: randEmotion4, userChoiceD: "Surprise", answer: 3))
+        list.append(Question(image: randAns5, questionText: "What emotion is this?", userChoiceA: "Happiness", userChoiceB: "Contempt", userChoiceC: "Anger", userChoiceD: randEmotion5, answer: 4))
     }
 }
--- SnapMoji/TestingViewController.swift
+++ SnapMoji/TestingViewController.swift
@@ -2,6 +2,15 @@
 //  TestingViewController.swift
 //  SnapMoji
 //
+//  Worked on by: Josh Baltar
+//
+//  Changelog:
+//  2.0 - Initial commit layout and segues finished
+//  2.1 - Generates working quiz template
+//  2.2 - Quizzes generated with sample images
+//
+//  Known bugs:
+//  Gallery does not save image when an image is selected
 //  Created by zza92 on 2018-11-08.
 //  Copyright © 2018 Mojo Mojis. All rights reserved.
 //
@@ -11,6 +20,8 @@ import UIKit
 
 class TestingViewController: UIViewController {
 
+    var difficulty = 0
+    var pickedAnswer = 0
     
     @IBOutlet weak var questionCounter: UILabel!
     @IBOutlet weak var friendName: NSLayoutConstraint!
@@ -45,17 +56,24 @@ class TestingViewController: UIViewController {
     }
     
     @IBAction func answerPressed(_ sender: UIButton) {
-        if sender.tag == selectedAnswer {
+        pickedAnswer =  sender.tag
+        
+    }
+    
+    @IBAction func submitAnswer(_ sender: UIButton) {
+        if pickedAnswer == selectedAnswer {
             print("correct")
             score += 1
+        } else if pickedAnswer == 0 {
+            print ("no answer selected")
         } else {
             print("wrong")
         }
         
         questionNumber += 1
         updateQuestion()
-        
     }
+    
     
     func updateQuestion(){
         if questionNumber <= allQuestions.list.count - 1 {
@@ -67,7 +85,8 @@ class TestingViewController: UIViewController {
             choiceD.setTitle(allQuestions.list[questionNumber].choiceD, for: UIControlState.normal)
             selectedAnswer = allQuestions.list[questionNumber].correctAnswer
         } else {
-            let alert = UIAlertController(title: "Nice", message: "You have finished the quiz!", preferredStyle: .alert)
+            let finishMessage = "You got \(score)/\(allQuestions.list.count)!"
+            let alert = UIAlertController(title: "Nice job!", message: finishMessage, preferredStyle: .alert)
             let restartAction = UIAlertAction(title: "Restart", style: .default, handler: {action in self.restartQuiz()})
             alert.addAction(restartAction)
             present(alert, animated:true, completion: nil)
